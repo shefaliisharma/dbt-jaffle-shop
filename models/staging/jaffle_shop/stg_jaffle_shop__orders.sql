2ef3b337e860cea50fe52b6982b62ead224d458f
Shefali Sharma <shefali.finance@gmail.com>
2026-08-20 20:00:51 +0000

    added generic data tests for jaffle_shop__orders

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,22 @@
-select
-        id as order_id,
-        user_id as customer_id, -- name all primary ids from vague to particular
-        order_date,
-        status
+with 
 
-    from {{source('jaffle_shop','orders')}}
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status as order_status,
+        _etl_loaded_at
+
+    from source
+
+)
+
+select * from renamed
